24f15539f385ec508999cfffed3ce51603eb4655
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-07-23 00:59:32 +0900

    Setup player position

--- game/app/main.rb
+++ game/app/main.rb
@@ -3,15 +3,18 @@ require 'lib/engine.rb'
 def tick(args)
   setup(args) if args.tick_count.zero?
 
-  $terminal.print(x: 1, y: 1, string: '@')
+  $terminal.print(x: args.state.player_x, y: args.state.player_y, string: '@')
   $terminal.render(args)
   render_framerate(args)
 end
 
-def setup(_args)
+def setup(args)
   # DragonRuby is fixed at 1280x720 so choosing a resolution that fits neatly
   screen_width = 80  # 80 * 16 = 1280
   screen_height = 45 # 45 * 16 = 720
+
+  args.state.player_x = screen_width.idiv(2)
+  args.state.player_y = screen_height.idiv(2)
 
   tileset = Engine::Tileset.new('Zilk-16x16.png')
   $terminal = Engine::Terminal.new(screen_width, screen_height, tileset: tileset)
