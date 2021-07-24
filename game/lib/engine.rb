7dc1a73ca6100eb5966795da5bef2613648eed3f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-07-24 23:20:10 +0900

    Engine: Add bresenham algorithm

--- game/lib/engine.rb
+++ game/lib/engine.rb
@@ -1,2 +1,3 @@
+require 'lib/engine/line_of_sight.rb'
 require 'lib/engine/tileset.rb'
 require 'lib/engine/terminal.rb'
---
+++ game/lib/engine/line_of_sight.rb
@@ -0,0 +1,36 @@
+
+module Engine
+  # Simulated terminal for rendering the tiles
+  module LineOfSight
+    class << self
+      def bresenham(start_point, end_point)
+        [].tap { |result|
+          x = start_point.x
+          y = start_point.y
+          end_x = end_point.x
+          end_y = end_point.y
+
+          dx = (end_x - x).abs
+          sx = x < end_x ? 1 : -1
+          dy = -(end_y - y).abs
+          sy = y < end_y ? 1 : -1
+          err = dx + dy
+
+          loop do
+            result << [x, y]
+            break if x == end_x && y == end_y
+
+            e2 = 2 * err
+            if e2 > dy
+              err += dy
+              x += sx
+            elsif e2 <= dx
+              err += dx
+              y += sy
+            end
+          end
+        }
+      end
+    end
+  end
+end
