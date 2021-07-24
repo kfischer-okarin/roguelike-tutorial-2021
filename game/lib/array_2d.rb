ce92f72cb85f4a0be8eda899c53f1295d6b23c6b
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-07-24 22:33:56 +0900

    Engine: Improve array 2d

--- game/lib/array_2d.rb
+++ game/lib/array_2d.rb
@@ -12,6 +12,58 @@ class Array2D
   end
 
   def []=(x, y, value)
-    @data[y * @w + x] = value
+    case value
+    when Array2D
+      assign_array_2d(x, y, value)
+    else
+      @data[y * @w + x] = value
+    end
+  end
+
+  def fill_rect(rect, value)
+    x = rect.x
+    y = rect.y
+    assigned_index = 0
+    assigned_w = rect.w
+    assigned_size = rect.w * rect.h
+
+    own_data = @data
+    own_w = @w
+    own_index = y * own_w + x
+    next_row_index = own_index + assigned_w
+
+    while assigned_index < assigned_size
+      own_data[own_index] = value
+      assigned_index += 1
+      own_index += 1
+      if own_index >= next_row_index
+        own_index = own_index - assigned_w + own_w
+        next_row_index += own_w
+      end
+    end
+  end
+
+  private
+
+  def assign_array_2d(x, y, array_2d)
+    assigned_index = 0
+    assigned_data = array_2d.data
+    assigned_size = assigned_data.size
+    assigned_w = array_2d.w
+
+    own_data = @data
+    own_w = @w
+    own_index = y * own_w + x
+    next_row_index = own_index + assigned_w
+
+    while assigned_index < assigned_size
+      own_data[own_index] = assigned_data[assigned_index]
+      assigned_index += 1
+      own_index += 1
+      if own_index >= next_row_index
+        own_index = own_index - assigned_w + own_w
+        next_row_index += own_w
+      end
+    end
   end
 end
