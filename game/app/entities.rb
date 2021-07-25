49f3cefc0f9c83dab117fb5f27ccdc6a79e94f75
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-07-25 22:25:35 +0900

    Refactor: Store entities in hash

--- game/app/entities.rb
+++ game/app/entities.rb
@@ -5,15 +5,15 @@ module Entities
     attr_accessor :gtk_state
 
     def setup
-      @gtk_state.entities = []
+      @gtk_state.entities_by_id = {}
     end
 
     def add(entity)
-      @gtk_state.entities << entity
+      @gtk_state.entities_by_id[entity.entity_id] = entity
     end
 
     def each(&block)
-      @gtk_state.entities.each(&block)
+      @gtk_state.entities_by_id.each_value(&block)
     end
 
     def player
