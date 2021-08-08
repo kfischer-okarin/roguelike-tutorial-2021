26b7804993e5137d73524733750e1cd21f14f6d9
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-08-08 23:35:22 +0900

    Engine: Add serialization/to string helpers to DataBackedObject

--- game/lib/data_backed_object.rb
+++ game/lib/data_backed_object.rb
@@ -1,23 +1,37 @@
 class DataBackedObject
-  def self.data_reader(*attributes)
-    attributes.each do |attribute|
-      define_method attribute do
-        data.send(attribute)
+  class << self
+    def data_reader(*attributes)
+      attributes.each do |attribute|
+        define_method attribute do
+          data.send(attribute)
+        end
       end
+    end
+
+    def data_writer(*attributes)
+      attributes.each do |attribute|
+        method = :"#{attribute}="
+        define_method method do |value|
+          data.send(method, value)
+        end
+      end
+    end
+
+    def data_accessor(*attributes)
+      data_reader(*attributes)
+      data_writer(*attributes)
     end
   end
 
-  def self.data_writer(*attributes)
-    attributes.each do |attribute|
-      method = :"#{attribute}="
-      define_method method do |value|
-        data.send(method, value)
-      end
-    end
+  def to_s
+    "#{self.class}(#{data.inspect})"
   end
 
-  def self.data_accessor(*attributes)
-    data_reader(*attributes)
-    data_writer(*attributes)
+  def inspect
+    to_s
+  end
+
+  def serialize
+    data
   end
 end
