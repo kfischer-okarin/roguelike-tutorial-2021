1778a07bb5b2c4f1f4229ea6d447661e4e37f158
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-08-09 12:56:35 +0900

    Go back to gameplay when pressing unknown input in item selection

--- game/app/scenes/item_selection.rb
+++ game/app/scenes/item_selection.rb
@@ -3,34 +3,48 @@ module Scenes
     def initialize(previous_scene, inventory:, &build_action_for_selected_item)
       @inventory = inventory
       @build_action_for_selected_item = build_action_for_selected_item
-      super()
       @previous_scene = previous_scene
+      super()
+    end
+
+    def go_back_to_previous_scene
+      $game.scene = @previous_scene
+    end
+
+    def after_action_performed
+      go_back_to_previous_scene
+      @previous_scene.after_action_performed
     end
 
     protected
 
     def build_input_handler
-      InputEventHandler.new(@inventory, @build_action_for_selected_item)
-    end
-
-    def after_action_performed
-      $game.scene = @previous_scene
-      @previous_scene.after_action_performed
+      InputEventHandler.new(self, @item_list, @build_action_for_selected_item)
     end
 
     class InputEventHandler < BaseInputHandler
-      def initialize(inventory, build_action_for_selected_item)
+      def initialize(selection_scene, selection_ui, build_action_for_selected_item)
         super()
-        @inventory = inventory
+        @selection_scene = selection_scene
+        @selection_ui = selection_ui
         @build_action_for_selected_item = build_action_for_selected_item
       end
 
       def dispatch_action_for_char_typed(event)
-        index = event.char.ord - 'a'.ord
-        selected_item = @inventory.items[index]
+        unless @selection_ui.valid_input_char? event.char
+          @selection_scene.go_back_to_previous_scene
+          return
+        end
+
+        selected_item = @selection_ui.item_for_char event.char
         return unless selected_item
 
         @build_action_for_selected_item.call(selected_item)
+      end
+
+      def dispatch_action_for_quit
+        @selection.go_back_to_previous_scene
+        nil
       end
     end
   end
--- game/tests/app/scenes/item_selection.rb
+++ game/tests/app/scenes/item_selection.rb
@@ -44,3 +44,47 @@ def test_item_selection_cannot_selected_non_existing_item(args, assert)
   assert.equal! selected_item, nil
   assert.true! $game.calls.empty?
 end
+
+def test_item_selection_non_item_input_returns_to_previous_scene(args, assert)
+  TestHelper.init_globals(args)
+  previous_scene = TestHelper::Spy.new
+  potion = TestHelper.build_item('Potion')
+  bomb = TestHelper.build_item('Bomb')
+  inventory = TestHelper.build_inventory(items: [potion, bomb])
+  selected_item = nil
+  scene = Scenes::ItemSelection.new(previous_scene, inventory: inventory) do |item|
+    selected_item = item
+  end
+
+  scene.handle_input_events(
+    [
+      { type: :char_typed, char: '3' }
+    ]
+  )
+
+  assert.equal! selected_item, nil
+  assert.includes! $game.calls, [:scene=, [previous_scene]]
+  assert.true! previous_scene.calls.empty?
+end
+
+def test_item_selection_quit_input_returns_to_previous_scene(args, assert)
+  TestHelper.init_globals(args)
+  previous_scene = TestHelper::Spy.new
+  potion = TestHelper.build_item('Potion')
+  bomb = TestHelper.build_item('Bomb')
+  inventory = TestHelper.build_inventory(items: [potion, bomb])
+  selected_item = nil
+  scene = Scenes::ItemSelection.new(previous_scene, inventory: inventory) do |item|
+    selected_item = item
+  end
+
+  scene.handle_input_events(
+    [
+      { type: :quit }
+    ]
+  )
+
+  assert.equal! selected_item, nil
+  assert.includes! $game.calls, [:scene=, [previous_scene]]
+  assert.true! previous_scene.calls.empty?
+end
