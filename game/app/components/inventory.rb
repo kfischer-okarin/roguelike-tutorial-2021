eb2103011871e73d42c13e5164c879faa82d6c6f
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-08-09 15:43:16 +0900

    Implement Inventory#drop

--- game/app/components/inventory.rb
+++ game/app/components/inventory.rb
@@ -4,8 +4,18 @@ module Components
       data.items << entity.id
     end
 
+    def remove_entity(entity)
+      data.items.delete entity.id
+    end
+
     def items
       data.items.map { |entity_id| Entities.get(entity_id) }
+    end
+
+    def drop(item)
+      owning_entity = entity
+      item.place owning_entity.game_map, x: owning_entity.x, y: owning_entity.y
+      $message_log.add_message(text: "You dropped the #{item.name}")
     end
   end
 end
--- game/tests/app/components/inventory.rb
+++ game/tests/app/components/inventory.rb
@@ -3,8 +3,8 @@ require 'tests/test_helper.rb'
 def test_inventory_add_entity(args, assert)
   TestHelper.init_globals(args)
   entity_data = { items: [] }
-  inventory = Components::Inventory.new(TestHelper.build_entity, entity_data)
-  item_entity = TestHelper.build_entity
+  inventory = Components::Inventory.new(TestHelper.build_actor, entity_data)
+  item_entity = TestHelper.build_item
 
   inventory.add_entity(item_entity)
 
@@ -13,9 +13,27 @@ end
 
 def test_inventory_items(args, assert)
   TestHelper.init_globals(args)
-  item_entity = TestHelper.build_entity
-  inventory = Components::Inventory.new(TestHelper.build_entity, items: [])
+  item_entity = TestHelper.build_item
+  inventory = Components::Inventory.new(TestHelper.build_actor, items: [])
   inventory.add_entity item_entity
 
   assert.equal! inventory.items, [item_entity]
+end
+
+def test_inventory_drop(args, assert)
+  TestHelper.init_globals(args)
+  actor = TestHelper.build_actor
+  game_map = TestHelper.build_map_with_entities(
+    [3, 4] => actor
+  )
+  inventory = Components::Inventory.new(actor, items: [])
+  item_entity = TestHelper.build_item('Ball')
+  item_entity.place inventory
+
+  inventory.drop item_entity
+
+  assert.equal! inventory.items, []
+  assert.includes! game_map.items, item_entity
+  assert.has_attributes! item_entity, x: 3, y: 4
+  assert.includes! TestHelper.log_messages, 'You dropped the Ball'
 end
