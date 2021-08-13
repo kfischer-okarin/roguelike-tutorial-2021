d336dfe32924ad4d7868302c5e3319c0159afc18
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-08-13 16:36:08 +0900

    Tests: Refactor with simpler game map factory

--- game/tests/app/entity/base_entity.rb
+++ game/tests/app/entity/base_entity.rb
@@ -11,19 +11,16 @@ def test_base_entity_place_in_game_map(_args, assert)
   assert.includes! game_map.entities, entity
 end
 
-def test_base_entity_place_in_inventory(_args, assert)
-  entity = build_entity
+def test_base_item_place_in_inventory(_args, assert)
+  item = build_item
   actor = build_actor
-  build_game_map_with_entities(
-    [4, 4] => entity,
-    [5, 5] => actor
-  )
+  build_game_map_with_entities(item, actor)
 
-  entity.place(actor.inventory)
+  item.place(actor.inventory)
 
-  assert.has_attributes! entity, x: nil, y: nil, parent: actor.inventory
-  assert.has_attributes! entity.data, parent: { type: :inventory, entity_id: actor.id }
-  assert.includes! actor.inventory.items, entity
+  assert.has_attributes! item, x: nil, y: nil, parent: actor.inventory
+  assert.has_attributes! item.data, parent: { type: :inventory, entity_id: actor.id }
+  assert.includes! actor.inventory.items, item
 end
 
 def test_base_entity_parent_inventory_from_data(_args, assert)
--- game/tests/app/scenes/gameplay.rb
+++ game/tests/app/scenes/gameplay.rb
@@ -5,11 +5,8 @@ def test_gameplay_scene_ai_impossible_actions_are_ignored(_args, assert)
   other_entity = build_actor
   error_ai = stub(perform_action: -> { raise Action::Impossible, 'Something went wrong' })
   stub_attribute(other_entity, :ai, error_ai)
+  $game.game_map = build_game_map_with_entities(player, other_entity)
 
-  $game.game_map = build_game_map_with_entities(
-    [5, 5] => player,
-    [1, 1] => other_entity
-  )
   scene = Scenes::Gameplay.new(player: player)
 
   assert.raises_no_exception! do
