869d3bebd9fedcc8af94eb98e16345b00bdd4b45
Kevin Fischer <kfischer_okarin@yahoo.co.jp>
2021-08-13 14:47:37 +0900

    Refactor: Move event dispatch logic up to BaseScene

--- game/app/scenes/base_input_handler.rb
+++ game/app/scenes/base_input_handler.rb
@@ -1,15 +1,4 @@
 module Scenes
   class BaseInputHandler
-    def handle_input_event(event)
-      method_name = :"dispatch_action_for_#{event.type}"
-      return unless respond_to? method_name
-
-      handler = method(method_name)
-      if handler.arity == 1
-        handler.call(event)
-      else
-        handler.call
-      end
-    end
   end
 end
--- game/app/scenes/base_scene.rb
+++ game/app/scenes/base_scene.rb
@@ -7,7 +7,15 @@ module Scenes
     end
 
     def handle_input_event(input_event)
-      input_event_handler.handle_input_event(input_event)
+      method_name = :"dispatch_action_for_#{input_event.type}"
+      return unless input_event_handler.respond_to? method_name
+
+      handler = input_event_handler.method(method_name)
+      if handler.arity == 1
+        handler.call(input_event)
+      else
+        handler.call
+      end
     end
 
     def handle_input_events(input_events)
