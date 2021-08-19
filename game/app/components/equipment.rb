86eea34c3396f970f907af1d183460f8103bde17
Kevin Fischer <kevin@agileware.jp>
2021-08-20 02:03:51 +0900

    Add unequip and messages

--- game/app/components/equipment.rb
+++ game/app/components/equipment.rb
@@ -11,12 +11,28 @@ module Components
     end
 
     def equip(item)
+      equipped_item = get_slot(item.equippable.slot)
+      unequip equipped_item if equipped_item
+
       set_slot(item.equippable.slot, item)
-      item.equippable.equipped_by = entity
+      $message_log.add_message(text: "You equip the #{item.name}.")
+    end
+
+    def unequip(item)
+      set_slot(item.equippable.slot, nil)
+      $message_log.add_message(text: "You remove the #{item.name}.")
     end
 
     def set_slot(slot, item)
+      previous_item = get_slot(slot)
+      previous_item.equippable.equipped_by = nil if previous_item
+
       instance_variable_set(:"@#{slot}", item)
+      item.equippable.equipped_by = entity if item
+    end
+
+    def get_slot(slot)
+      instance_variable_get(:"@#{slot}")
     end
   end
 end
--- game/tests/app/components/equipment.rb
+++ game/tests/app/components/equipment.rb
@@ -8,16 +8,41 @@ def test_equipment_equip_weapon(_args, assert)
 
   assert.equal! actor.equipment.weapon, item
   assert.equal! item.equippable.equipped_by, actor
+
+  actor.equipment.unequip(item)
+
+  assert.equal! actor.equipment.weapon, nil
+  assert.equal! item.equippable.equipped_by, nil
 end
 
 def test_equipment_equip_armor(_args, assert)
   actor = build_actor
-  item = build_item(equippable: { slot: :armor })
+  item = build_item(name: 'Fire Armor', equippable: { slot: :armor })
 
   actor.equipment.equip(item)
 
   assert.equal! actor.equipment.armor, item
   assert.equal! item.equippable.equipped_by, actor
+  assert.includes! log_messages, 'You equip the Fire Armor.'
+
+  actor.equipment.unequip(item)
+
+  assert.equal! actor.equipment.armor, nil
+  assert.equal! item.equippable.equipped_by, nil
+  assert.includes! log_messages, 'You remove the Fire Armor.'
+end
+
+def test_equipment_equip_unequips_previous_equipment(_args, assert)
+  actor = build_actor
+  armor1 = build_item(equippable: { slot: :armor })
+  armor2 = build_item(equippable: { slot: :armor })
+
+  actor.equipment.equip(armor1)
+  actor.equipment.equip(armor2)
+
+  assert.equal! actor.equipment.armor, armor2
+  assert.equal! armor2.equippable.equipped_by, actor
+  assert.equal! armor1.equippable.equipped_by, nil
 end
 
 def test_equipment_power_bonus(_args, assert)
@@ -44,4 +69,17 @@ def test_equipment_defense_bonus(_args, assert)
   actor.equipment.equip(armor)
 
   assert.equal! actor.equipment.defense_bonus, 5
+end
+
+def test_equipment_equipped(_args, assert)
+  actor = build_actor
+  weapon = build_item(equippable: { slot: :weapon, defense_bonus: 3 })
+  armor = build_item(equippable: { slot: :armor, defense_bonus: 2 })
+  unequipped = build_item(equippable: { slot: :armor, defense_bonus: 2 })
+  actor.equipment.equip(weapon)
+  actor.equipment.equip(armor)
+
+  assert.true! actor.equipment.equipped?(weapon)
+  assert.true! actor.equipment.equipped?(armor)
+  assert.false! actor.equipment.equipped?(unequipped)
 end
